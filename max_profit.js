29e3505998842404899b625b08f1857dd98bf7dd
SumitabhBraincade <sumitabh@braincade.in>
2024-05-13 09:30:08 +0530

    worked_on_printing_the_property_purchased

--- max_profit.js
+++ max_profit.js
@@ -1,35 +1,37 @@
 const propertyEarning = {
-    "Theatre": 1500,
-    "Pub": 1000,
-    "Commercial Park": 3000
+  Theatre: 1500,
+  Pub: 1000,
+  "Commercial Park": 3000,
 };
 
 const buildTimes = {
-    "Theatre": 5,
-    "Pub": 4,
-    "Commercial Park": 10
+  Theatre: 5,
+  Pub: 4,
+  "Commercial Park": 10,
 };
 
-let maxProfit = [];
+let maxProfit = {};
 
 function max_profit(n, totalEarning, propertyCreated) {
-    if (n === 0) {
-        maxProfit.push(totalEarning);
-        return;
-    }
+  if (n === 0) {
+    maxProfit[totalEarning] = propertyCreated;
+    return;
+  }
 
-    for (const [name, earning] of Object.entries(propertyEarning)) {
-        if (buildTimes[name] <= n) {
-            const remainingTime = n - buildTimes[name];
-            const newPropertyCreated = { ...propertyCreated };
-            newPropertyCreated[name] += 1;
-            const newTotalEarning = totalEarning + earning * remainingTime;
-            maxProfit.push(newTotalEarning);
-            max_profit(remainingTime, newTotalEarning, newPropertyCreated);
-        }
+  for (const [name, earning] of Object.entries(propertyEarning)) {
+    if (buildTimes[name] <= n) {
+      const remainingTime = n - buildTimes[name];
+      const newPropertyCreated = { ...propertyCreated };
+      newPropertyCreated[name] += 1;
+      const newTotalEarning = totalEarning + earning * remainingTime;
+      maxProfit[newTotalEarning] = newPropertyCreated;
+      max_profit(remainingTime, newTotalEarning, newPropertyCreated);
     }
+  }
 }
 
-max_profit(13, 0, { "Theatre": 0, "Pub": 0, "Commercial Park": 0 });
+max_profit(13, 0, { Theatre: 0, Pub: 0, "Commercial Park": 0 });
 
-console.log(Math.max(...maxProfit));
+const max_earning = Math.max(...Object.keys(maxProfit).map((e) => Number(e)));
+
+console.log(max_earning, maxProfit[max_earning.toString()]);
